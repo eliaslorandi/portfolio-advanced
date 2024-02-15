438427067bd59d360782c2c7e6dc9eae27f1687a
Elias Lorandi <elias.lorandi@hotmail.com>
2024-02-15 14:37:11 -0300

    Instalação e configuração do JUI DatePicker

--- backend/views/project/_form.php
+++ backend/views/project/_form.php
@@ -18,9 +18,13 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
 
-    <?= $form->field($model, 'start_date')->textInput() ?>
+    <?= $form->field($model, 'start_date')->widget(\yii\jui\DatePicker::class, [
+        //'language' => 'pt-BR',
+    ]) ?>
 
-    <?= $form->field($model, 'end_date')->textInput() ?>
+    <?= $form->field($model, 'end_date')->widget(\yii\jui\DatePicker::class, [
+        //'language' => 'pt-BR' 
+    ]) ?>
 
     <div class="form-group">
         <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
